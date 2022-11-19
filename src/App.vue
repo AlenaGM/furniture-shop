bbf34c0a5fb829dc46d57c7082a970452aaa0c81
AlenaGM <alena.guillaume4@gmail.com>
2022-11-19 21:53:49 +0100

    project setup

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import Header from "./components/layout/Header.vue";
+</script>
+
+<template>
+  <div class="container">
+    <Header></Header>
+  </div>
+</template>
+
+<style lang="scss">
+@import url("https://fonts.cdnfonts.com/css/satoshi");
+@import url("https://fonts.cdnfonts.com/css/clash-display");
+
+* {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+  font-family: "Satoshi", sans-serif;
+}
+
+.container {
+  max-width: 1440px;
+  margin: 0 auto;
+}
+</style>
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,55 @@
+<template>
+  <header class="header">
+    <div class="header-top">
+      <div class="header-top-search">
+        <img src="/svg/header-search.svg" alt="search" />
+      </div>
+      <a href="#" class="header-logo">Avion</a>
+      <div class="header-top-right">
+        <div href="#" class="header-top-right__cart">
+          <a href="#">
+            <img src="/svg/header-cart.svg" alt="cart" />
+          </a>
+        </div>
+        <div href="#" class="header-top-right__user">
+          <a href="#">
+            <img src="/svg/header-user.svg" alt="usert" />
+          </a>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+.header {
+  height: 132px;
+  background: #fff;
+  &-top {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    height: 70px;
+    align-items: center;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+    margin: 0 28px;
+    &-right {
+      display: flex;
+      align-items: center;
+      justify-content: flex-end;
+      &__cart {
+        margin-right: 16px;
+      }
+    }
+  }
+  &-logo {
+    color: #22202e;
+    font-size: 24px;
+    text-decoration: none;
+    font-family: "Clash Display", sans-serif;
+    text-align: center;
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+}
+</style>
