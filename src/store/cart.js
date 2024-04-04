3f9bf6a2aa092d86c32c54a5ea809d667d0bf258
Alena Guillaume <alena.guillaume4@gmail.com>
2024-04-04 22:26:49 +0200

    added localstorage to cart

--- src/store/cart.js
+++ src/store/cart.js
@@ -1,30 +1,101 @@
 import { defineStore } from "pinia";
+import { ref, computed, watch } from "vue";
 
-export const useCartStore = defineStore("cartStore", {
-  state: () => ({
-    cart: [],
-  }),
-  actions: {
-    addToCart(product, count) {
-      const cartIndex = this.cart.findIndex((el) => el.id === product.id);
-      if (cartIndex === -1) {
-        this.cart.push({ ...product, count });
-      } else if (this.cart[cartIndex].count + count <= product.stock) {
-        this.cart[cartIndex].count += count;
-      }
+export const useCartStore = defineStore("cartStore", () => {
+  const cart = ref([]);
+  let counter = ref(1);
+
+  const cartInLocalStorage = localStorage.getItem("cart");
+  if (cartInLocalStorage) {
+    cart.value = JSON.parse(cartInLocalStorage)._value;
+  }
+
+  const cartTotalItems = computed(() => {
+    let totalItems = 0;
+    for (let item of cart.value) {
+      totalItems += item.count;
+    }
+    return totalItems;
+  });
+
+  const cartTotalPrice = computed(() => {
+    let totalPrice = 0;
+    for (let item of cart.value) {
+      totalPrice += item.price * item.count;
+    }
+    return totalPrice;
+  });
+
+  const addToCart = (product, count) => {
+    const cartIndex = cart.value.findIndex((el) => el.id === product.id);
+    if (cartIndex === -1) {
+      cart.value.push({ ...product, count });
+    } else if (cart.value[cartIndex].count + count <= product.stock) {
+      cart.value[cartIndex].count += count;
+    }
+  };
+
+  const addItem = (index, stock) => {
+    cart.value[index].count >= stock
+      ? (cart.value[index].count = stock)
+      : cart.value[index].count++;
+  };
+
+  const deleteItem = (index) => {
+    cart.value[index].count === 1
+      ? cart.value.splice(index, 1)
+      : cart.value[index].count--;
+  };
+
+  const deleteFromCart = (index) => {
+    cart.value.splice(index, 1);
+  };
+
+  watch(
+    () => cart,
+    (state) => {
+      localStorage.setItem("cart", JSON.stringify(state));
     },
-    addItem(index, stock) {
-      this.cart[index].count >= stock
-        ? (this.cart[index].count = stock)
-        : this.cart[index].count++;
-    },
-    deleteItem(index) {
-      this.cart[index].count === 1
-        ? this.cart.splice(index, 1)
-        : this.cart[index].count--;
-    },
-    deleteFromCart(index) {
-      this.cart.splice(index, 1);
-    },
-  },
+    { deep: true }
+  );
+
+  return {
+    cart,
+    counter,
+    cartTotalItems,
+    cartTotalPrice,
+    addToCart,
+    addItem,
+    deleteItem,
+    deleteFromCart,
+  };
 });
+
+//export const useCartStore = defineStore("cartStore", {
+//  state: () => ({
+//    cart: [],
+//  }),
+//  actions: {
+//    addToCart(product, count) {
+//      const cartIndex = this.cart.findIndex((el) => el.id === product.id);
+//      if (cartIndex === -1) {
+//        this.cart.push({ ...product, count });
+//      } else if (this.cart[cartIndex].count + count <= product.stock) {
+//        this.cart[cartIndex].count += count;
+//      }
+//    },
+//    addItem(index, stock) {
+//      this.cart[index].count >= stock
+//        ? (this.cart[index].count = stock)
+//        : this.cart[index].count++;
+//    },
+//    deleteItem(index) {
+//      this.cart[index].count === 1
+//        ? this.cart.splice(index, 1)
+//        : this.cart[index].count--;
+//    },
+//    deleteFromCart(index) {
+//      this.cart.splice(index, 1);
+//    },
+//  },
+//});
